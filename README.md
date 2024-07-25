97429bc3338b25c6f447c5dfa5c831605f4eda05
JamesBara <JamesBara@github.com>
2024-07-25 19:29:01 +0300

    Add project files.

---
+++ README.md
@@ -0,0 +1,38 @@
+﻿# Port of CMSIS-DAP for STM32L412.
+
+CMSIS-DAP has been stripped down to only 2 pin Serial Wire Debugging and the nReset pin.
+The bare minimum Microsoft OS 2.0 descriptor has been added to provide PnP capabilities on Windows 8.1 and above.
+
+The project enumerates properly on Windows 11. It should work with OpenOCD and arm-none-eabi-gdb. For now it remains untested.
+
+This project depends on [STM32L4xx_USB_Device](https://github.com/JamesBara/STM32L4xx_USB_Device), [STM32L4xx](https://github.com/JamesBara/STM32L4xx), [CMSIS-DAP](https://github.com/ARM-software/CMSIS-DAP) and [CMSIS6](https://github.com/ARM-software/CMSIS_6).
+
+Hardware Pinout:
+```
+/*@todo but for testing purposes it's the following.*/
+PA0 nRESET 
+PA1 SWCLK
+PA2 SWDIO
+```
+
+File tree:
+```
+STM32L412_CMSIS_DAP
+├───CMSIS-DAP
+├───STM32L4xx_USB_Device
+│      └───STM32L4xx
+│            └───CMSIS_6
+├───inc
+│    ├───bsp.h
+│    ├───DAP_config.h
+│    └───ms_os_20_request.h
+├───src
+│    ├───bsp.c
+│    └───main.c
+├───STM32L412.ld
+├───CMakeLists.txt
+├───CMakePresets.json
+├───gcc-arm-none-eabi.cmake
+├───LICENSE.txt
+└───README.md
+```
---
+++ inc/ms_os_20_request.h
@@ -0,0 +1,69 @@
+#ifndef MS_OS_20_REQUEST_H
+#define MS_OS_20_REQUEST_H
+
+#include <stdint.h>
+#include "stm32l4xx.h"
+
+#define MS_OS_20_PLATFORM 0x5
+
+#define MS_OS_20_LENGTH_SET_HEADER 10
+#define MS_OS_20_LENGTH_FEATURE_COMPATIBLE_ID 20
+
+
+#define MS_OS_20_WINDOWS_VERSION_81 0x06030000UL
+
+#define MS_OS_20_SET_HEADER_DESC 0
+#define MS_OS_20_FEATURE_COMPATIBLE_ID_DESC 3
+#define MS_OS_20_FEATURE_REGISTER_PROPERTY_DESC 4
+
+#define MS_OS_20_REG_SZ 1
+
+#define MS_OS_20_CUSTOM_REQUEST 0x1U
+
+
+#define MS_OS_20_DESCRIPTOR_INDEX 0x7
+#define MS_OS_20_SET_ALT_ENUMERATION 0x8
+
+
+/************************************************
+ * Microsoft OS 2.0 Descriptor Set Information 
+ ***********************************************/
+/*CapabilityData of the MS OS 2.0 descriptor.*/
+typedef struct
+{
+    uint32_t dwWindowsVersion;
+    uint16_t wMSOSDescriptorSetTotalLength;
+    uint8_t bMS_VendorCode;
+    uint8_t bAltEnumCode;
+}__PACKED usbd_ms_os20_set_information_descriptor_type;
+
+typedef struct
+{
+	uint16_t wLength;
+	uint16_t wDescriptorType;
+	uint32_t dwWindowsVersion;
+	uint16_t wTotalLength;
+}__PACKED usbd_ms_os20_set_header_type;
+
+typedef struct
+{
+	uint16_t wLength;
+	uint16_t wDescriptorType;
+	uint8_t CompatibleID[8];
+	uint8_t SubCompatibleID[8];
+}__PACKED usbd_ms_os20_compatible_id_feature_type;
+
+typedef struct
+{
+	uint16_t wLength;
+	uint16_t wDescriptorType;
+	uint16_t wPropertyDataType;
+	uint16_t wPropertyNameLength;
+	uint8_t PropertyName[40];
+	uint16_t wPropertyDataLength;
+	uint8_t PropertyData[78];
+}__PACKED usbd_ms_os20_registry_property_feature_type;
+
+
+
+#endif /*MS_OS_20_REQUEST_H*/
